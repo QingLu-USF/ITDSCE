20480e16b062276e794b51b19487fcb4aa67ee6e
QingLu-USF <qlu@usf.edu>
2021-05-14 18:35:14 -0400

    Initial ITDSCE package, with main breaks data.

---
+++ R/hello.R
@@ -0,0 +1,23 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+#library(devtools)
+# Main breaks data in the City of Tampa (1999-2019)
+#Mainbreak = read.csv('C:/USF20200730/Project/CRISP/FromShihab/PythonCode/PipeFailure.csv')
+#use_data(Mainbreak)
